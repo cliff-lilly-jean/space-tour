a7e8b776e739e7529ce561fc0d72f6b3494751a2
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2022-01-04 01:18:30 -0500

    Added tailwind.css to the project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 
 function App() {
  return (
-  <div className="app"></div>
+  <div className="app">
+   <h1 className='text-center font-bold text-8xl'>Hello World</h1>
+  </div>
  );
 }
 
