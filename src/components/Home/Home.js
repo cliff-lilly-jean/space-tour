cb49c11f2845fdf6b20b2117cc9fb0680a2da28e
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2022-01-07 12:06:54 -0500

    Added responsiveness to the home page and the navbar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,16 +7,16 @@ import './Home.css';
 
 const Home = () => {
  return (
-  <div className='home flex flex-col justify-between items-center pt-6 pb-6 ml-auto mr-auto h-screen'>
+  <div className='home flex flex-col justify-between items-center md:pt-0 ml-auto mr-auto h-screen'>
    <Navbar />
-   <div className="container flex flex-col justify-between text-center lg:text-left md:items-center lg:flex-row max-w-6xl mb-32 p-5 m-auto">
-    <div className="container__left max-w-[327px] md:max-w-lg">
+   <div className="container flex flex-col justify-between h-full items-center text-center lg:text-left  lg:flex-row max-w-6xl mb-32 p-5 m-auto">
+    <div className="container__left mt-20 mb-32 max-w-[327px] md:mt-0 md:mb-10 md:max-w-lg">
      <h5 className='font-barlow text-[16px] md:text-[20px] lg:text-h5 text-secondary tracking-[4.72px]'>SO, YOU WANT TO TRAVEL TO</h5>
      <h1 className='text-white text-[80px] md:text-h1'>SPACE</h1>
-     <p className='text-secondary text-[15px] md:text-base lg:text-lg leading-8'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+     <p className='text-secondary text-[15px] max-w-[380px] ml-auto mr-auto md:text-[16px] md:max-w-[24em] lg:text-lg leading-8'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
     </div>
-    <div className="container__right w-[150px] h-[150px] md:w-[242px] md:h-[242px] lg:w-[274px] lg:h-[274px]">
-     <img className='' src={exploreIcon} alt="A circle with the word explore in the center" />
+    <div className="container__right mt-0 w-[150px] md:w-[242px] lg:w-[274px]">
+     <img src={exploreIcon} alt="A circle with the word explore in the center" />
     </div>
    </div>
   </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from "../../assets/imgs/shared/logo.png";
+import hamburgerNav from "../../assets/imgs/shared/icon-hamburger.svg";
 
 const Navbar = () => {
  return (
-  <nav className='navbar w-full mt-6'>
-   <div className="navbar__container flex flex-col md:flex-row justify-between items-center w-full">
-    <div className="nav__left w-[48] h-[48]">
+  <nav className='navbar w-full mt-8 mb-8 md:mt-0 lg:mt-8'>
+   <div className="navbar__container flex flex-row justify-between items-center w-full">
+    <div className="nav__left w-[48] pl-9">
      <img src={logo} alt="the space app logo" />
     </div>
-    <div className="nav__right flex  w[900px] pt-6 pb-6 pl-[100px] pr-[100px] text-white p-3 backdrop-blur-md">
-     <Link className='pl-6 pr-6 font-barlow tracking-[2.75px]' to="/">00 Home</Link>
-     <Link className='pl-6 pr-6 font-barlow tracking-[2.75px]' to="/destination">01 Destination</Link>
-     <Link className='pl-6 pr-6 font-barlow tracking-[2.75px]' to="/crew">02 Crew</Link>
-     <Link className='pl-6 pr-6 font-barlow tracking-[2.75px]' to="/technology">03 Technology</Link>
+    <div className="nav__right flex w[900px] pr-9 md:pt-7 md:pb-7 md:pl-[20px] md:pr-[20px] text-white p-3 md:backdrop-blur-lg">
+     <div className="nav__links hidden md:block">
+      <Link className='pl-6 pr-6 font-barlow tracking-[2.75px]' to="/"><span className='hidden lg:inline-block'>00</span> Home</Link>
+      <Link className='pl-6 pr-6 font-barlow tracking-[2.75px]' to="/destination"><span className='hidden lg:inline-block'>01</span> Destination</Link>
+      <Link className='pl-6 pr-6 font-barlow tracking-[2.75px]' to="/crew"><span className='hidden lg:inline-block'>02</span> Crew</Link>
+      <Link className='pl-6 pr-6 font-barlow tracking-[2.75px]' to="/technology"><span className='hidden lg:inline-block'>03</span> Technology</Link>
+     </div>
+     <div className="nav__hamburger md:hidden">
+      <img src={hamburgerNav} alt="" />
+     </div>
     </div>
    </div>
   </nav>
