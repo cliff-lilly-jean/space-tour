81149e4cce391383f94adfe809aa8540938b3ec5
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2022-01-08 13:51:04 -0500

    added comments on how to go about creating the destinations

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -9,7 +9,10 @@ const Destination = () => {
  return (
   <div className='destination'>
    <Navbar />
-
+   {/* todo: need to use the react-router to cycle through each destination */}
+   {/* on mobile, and add hover and active states. */}
+   {/* Need to create a new component; DestinationNav to pull data from the data.js file, */}
+   {/* and populate the destination page with the correct destination on active state */}
   </div>
  );
 };
