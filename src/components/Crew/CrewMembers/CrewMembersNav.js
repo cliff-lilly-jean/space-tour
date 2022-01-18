9bb262322ec2d677c310b2392efdd23698bced12
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2022-01-18 12:45:46 -0500

    build: created the Douglas crew member page

--- src/components/Crew/CrewMembers/CrewMembersNav.js
+++ src/components/Crew/CrewMembers/CrewMembersNav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Dot from '../../../assets/imgs/circle-solid.svg';
 
 // css
 import '../Crew.css';
@@ -8,7 +9,7 @@ function CrewMembersNav() {
  return (
   <div className='crew-members text-white'>
    <div className="crew-members__links">
-    <Link className='crew-members__link' to="/crew/douglas">Douglas</Link>
+    <Link className='crew-members__link' to="/crew/douglas"><img className='white' src={Dot} alt="" /></Link>
     <Link className='crew-members__link' to="/crew/mark">Mars</Link>
     <Link className='crew-members__link' to="/crew/victor">Europa</Link>
     <Link className='crew-members__link' to="/crew/anousheh">Titan</Link>
